4e79b57b95886b6e1badf23e30f8df004f24bcc4
kwinkich <vankord21@gmail.com>
2023-03-24 23:21:12 +0300

    start add switch theme

--- js/scripts.js
+++ js/scripts.js
@@ -9,3 +9,38 @@ document.querySelectorAll('.nav-link').forEach(function (link) {
         document.querySelector('.header__menu-block').classList.remove('open');
     });
 });
+
+
+
+//document.querySelector('.dark-light').addEventListener('click', (event) => {
+//    event.preventDefault();
+//    if (localStorage.getItem('theme') === 'dark') {
+//        localStorage.removeItem('theme');
+//    }
+//    else {
+//        localStorage.setItem('theme', 'dark')
+//    }
+//    addDarkClassToHTML();
+//});
+
+//function addDarkClassToHTML() {
+//    try {
+//        if (localStorage.getItem('theme') === 'dark') {
+//            document.querySelector('html').classList.add('dark');
+//        }
+
+//        else {
+//            document.querySelector('html').classList.remove('dark');
+//        }
+//    } catch (err) { }
+//}
+
+//addDarkClassToHTML();
+
+const handleChange = (isChecked) => {
+    if (isChecked) {
+        document.body.setAttribute('dark', '');
+    } else {
+        document.body.removeAttribute('dark');
+    }
+}
